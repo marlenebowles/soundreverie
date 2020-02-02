64f65f03a15cf9c0497d940edba6a1f8f048dcc6
Marlene Bowles <marlenebowles@gmail.com>
2020-02-02 16:25:14 -0500

    Init commit with setup

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	setupFiles: ['<rootDir>/jest.setup.js'],
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	globals: {
+		// we must specify a custom tsconfig for tests because we need the typescript transform
+		// to transform jsx into js rather than leaving it jsx such as the next build requires.  you
+		// can see this setting in tsconfig.jest.json -> "jsx": "react"
+		'ts-jest': {
+			tsConfig: 'tsconfig.jest.json',
+		},
+	},
+};
